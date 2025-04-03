c56e62c4d269492ea710fe8c05ed84776aebdcc4
7dong7 <tmdkdl777@gmail.com>
2025-04-03 20:54:48 +0900

    경로에 없는 페이지 생성 (잘못된 접근, 요청 페이지)

--- src/pages/Notfound.jsx
+++ src/pages/Notfound.jsx
@@ -1,8 +1,19 @@
+// css
+import "./Notfound.css";
 
 const Notfound = () => {
 
     return (
-        <div>Notfound</div>
+        <div className={"Notfound"}>
+            <div className={"Notfound-left"}>
+                <span className={"Notfound-bold"}>잘못된 접근</span>
+                <span>이거나 요청하신</span>
+            </div>
+            <div className={"Notfound-left"}>
+                <span>페이지를 </span>
+                <span className={"Notfound-bold"}>찾을 수 없습니다</span>
+            </div>
+        </div>
     )
 }
 
